e82f9dd1e9b80cda76ab1c7b49c4f33da419327c
Leon <jprothwell@gmail.com>
2018-10-16 15:32:58 +0800

    0.0.2

---
+++ UIDeviceX.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+
+// MARK:  sysetem version
+public struct iOSVersion {
+    
+    public static let IOS7Before   = UIDevice.SYSTEM_VERSION_LESS_THAN("7.0")
+    
+    public static let IOS7         = UIDevice.SYSTEM_VERSION_LESS_THAN("8.0") && UIDevice.SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO("7.0")
+    public static let IOS8         = UIDevice.SYSTEM_VERSION_LESS_THAN("9.0") && UIDevice.SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO("8.0")
+    public static let IOS9         = UIDevice.SYSTEM_VERSION_LESS_THAN("10.0") && UIDevice.SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO("9.0")
+    
+    public static let IOS9Later    = UIDevice.SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO("9.0")
+    
+}
+
+/** Model Extends UIDevice */
+public extension UIDevice {
+    
+    //systeme version compare methods
+    
+    public class func SYSTEM_VERSION_EQUAL_TO(_ version: String) -> Bool {
+        return self.current.systemVersion.compare(version as String,
+                                                  options: .numeric) == .orderedSame
+    }
+    
+    public class func SYSTEM_VERSION_GREATER_THAN(_ version: String) -> Bool {
+        return self.current.systemVersion.compare(version as String,
+                                                  options: .numeric) == .orderedDescending
+    }
+    
+    public class func SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(_ version: String) -> Bool {
+        return self.current.systemVersion.compare(version as String,
+                                                  options: .numeric) != .orderedAscending
+    }
+    
+    public class func SYSTEM_VERSION_LESS_THAN(_ version: String) -> Bool {
+        return UIDevice.current.systemVersion.compare(version as String,
+                                                      options: .numeric) == .orderedAscending
+    }
+    
+    public class func SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(_ version: String) -> Bool {
+        return self.current.systemVersion.compare(version as String,
+                                                  options: .numeric) != .orderedDescending
+    }
+    
+}
--- Utilites/Classes/Utilites.swift
+++ Utilites/Classes/Utilites.swift
@@ -1,13 +1,25 @@
 import Foundation
 import UIKit
 
-let kUIStatusBarHeight = Float(UIApplication.shared.statusBarFrame.size.height)
+public let kStatusBarHeight = Float(UIApplication.shared.statusBarFrame.size.height)
 
-let kApplication = UIApplication.shared
-let kAppKeyWindow = kApplication.keyWindow
-let kAppDelegate = kApplication.delegate
-let kAppNotificationCenter = NotificationCenter.default
-let kAppRootViewController = kAppDelegate?.window??.rootViewController
+public let kApplication = UIApplication.shared
+public let kAppKeyWindow = kApplication.keyWindow
+public let kAppDelegate = kApplication.delegate
+public let kAppNotificationCenter = NotificationCenter.default
+public let kAppRootViewController = kAppDelegate?.window??.rootViewController
 
-let kAppleLanguage = (UserDefaults.standard.array(forKey: "AppleLanguages")?.first as? String) ?? ""
-let kAppVersion = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? ""
+public let kAppleLanguage = (UserDefaults.standard.array(forKey: "AppleLanguages")?.first as? String) ?? ""
+public let kAppVersion = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? ""
+
+public let kScreenSize = UIScreen.main.bounds.size
+public let kScreenScale = Float(UIScreen.main.scale)
+public let kScreenWidth = Float(kScreenSize.width)
+public let kScreenHeight = Float(kScreenSize.height)
+
+
+public func PixelToFloat(_ p:Float) -> Float{
+    return p/kScreenScale
+}
+
+
